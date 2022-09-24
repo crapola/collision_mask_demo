f8f9766cde608aa9128256d692ec3da812c06023
Crapola <crapola@users.noreply.github.com>
2022-09-24 16:16:04 +0200

    Add monitoring checkboxes

--- area.gd
+++ area.gd
@@ -6,13 +6,20 @@ const COLOR_ON=Color.red
 onready var color_rect=$Control/ColorRect
 onready var hbox_layer=$"%HBox Layer"
 onready var hbox_mask=$"%HBox Mask"
+onready var check_monitorable=$"%CheckBox Monitorable"
+onready var check_monitoring=$"%CheckBox Monitoring"
 
 func _ready():
 	update_collision_bits(null)
+	check_monitorable.pressed=monitorable
+	check_monitoring.pressed=monitoring
 	var c=get_checkboxes()
 	for a in c:
 		for n in a:
 			n.connect("toggled",self,"update_collision_bits")
+	# Adjust collision shape size to control.
+	$CollisionShape2D.position=$Control.rect_size/2.0
+	$CollisionShape2D.shape.extents=$Control.rect_size/2.0
 
 # Retrieve checkbox controls.
 func get_checkboxes()->Array:
@@ -38,8 +45,22 @@ func _on_area_exited(_area):
 
 # Dragging.
 var grab_pos:Vector2=Vector2.UP
-func _on_VBoxContainer_gui_input(event):
+func _on_container_gui_input(event):
 	if event is InputEventMouseButton:
 		grab_pos=event.position if event.pressed else Vector2.UP
 	if event is InputEventMouseMotion and grab_pos!=Vector2.UP:
 		position=position+event.position-grab_pos
+
+func _on_CheckBox_Monitorable_toggled(button_pressed):
+	monitorable=button_pressed
+	#print_debug(self," monitorable is now ",monitorable)
+	_force_physics_update()
+
+func _on_CheckBox_Monitoring_toggled(button_pressed):
+	monitoring=button_pressed
+	#print_debug(self," monitoring is now ",monitoring)
+	_force_physics_update()
+
+func _force_physics_update():
+	$CollisionShape2D.disabled=true
+	$CollisionShape2D.disabled=false
